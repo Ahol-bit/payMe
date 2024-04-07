1e222892de0421a8bac435b3c8b081121998b57f
Ahol-bit <aholili27@gmail.com>
2024-04-07 15:41:40 +0700

    I think this is good, I think

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Remove or comment out the 'images' key under the "experimental" section
-  // basePath: "/payMe",
-  // output: "export",  // <=== enables static exports
-  // reactStrictMode: true,
+  basePath: "/payMe",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
