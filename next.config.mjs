d0eb20fa758f95b7545730c3f64aa6498bc21fbc
Ahol-bit <aholili27@gmail.com>
2024-04-07 16:59:22 +0700

    not nessecary

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   // Remove or comment out the 'images' key under the "experimental" section
   // basePath: "/payMe",
   // output: "export",  // <=== enables static exports
-  reactStrictMode: true,
+  // reactStrictMode: true,
 };
 
 export default nextConfig;
